f35547862ea24434e4911142b40fb72c69b11935
Tsiory Mialinarivo <tsiorymanana7@gmail.com>
2026-04-04 16:35:05 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionBF',
+  webDir: 'dist'
+};
+
+export default config;
