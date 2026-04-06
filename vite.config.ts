247cdfdc0b3b71b741635357a2d6891117ff781b
Tsiory Mialinarivo <tsiorymanana7@gmail.com>
2026-04-06 14:29:53 +0300

    Chunks visualisation

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    visualizer({
+      filename: 'dist/stats.html',
+      open: false,
+    }),
   ],
   resolve: {
     alias: {
